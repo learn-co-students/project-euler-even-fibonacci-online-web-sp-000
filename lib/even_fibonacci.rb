619a3d3067f0a819a2e255ef2f0f9617423b8dbc
Yvonne <yvonnersz@gmail.com>
2020-11-12 23:10:12 -0800

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def fibonacci
+    fibonacci_array = [1,2]
+
+    while fibonacci_array.length < 100
+        new_number = fibonacci_array[fibonacci_array.length - 2] + fibonacci_array[fibonacci_array.length - 1]
+        fibonacci_array.push(new_number)
+    end
+
+    fibonacci_array
+end
+
+def even_fibonacci_sum(limit)
+    new_fibonacci = fibonacci.reject {|number| number > limit}
+    even_fibonacci = new_fibonacci.reject {|number| number.odd?}
+    even_fibonacci.sum
+end
