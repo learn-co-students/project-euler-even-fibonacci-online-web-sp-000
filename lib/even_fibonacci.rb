06823ec2a06007a3fd067a9bed43ad064af16080
Cole Rau <colevrau@gmail.com>
2021-01-20 20:56:08 -0800

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(limit)
+  array = [1, 2]
+  sum = 3 
+  array.push(sum)
+  i = 4
+  while i <= limit
+    new_value = array[-1] + array[-2]
+    array.push(new_value)
+    i = new_value
+  end
+  even_array = array.filter { |num| num % 2 == 0 }
+  even_array.reduce(0) {|accu, num| accu + num}
+end
